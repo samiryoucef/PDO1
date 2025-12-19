dce50ae99f219ad61a1a8139f005cfb954e9ef0b
Youcef Samir <syoucef@MacBook-Pro-de-Youcef.local>
2025-12-19 21:00:22 +0100

    ajout d'un script

---
+++ scriptsSql/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO livres (titre, auteur, annee) VALUES
+('L’Étranger', 'Albert Camus', 1942),
+('1984', 'George Orwell', 1949),
+('Le Petit Prince', 'Antoine de Saint-Exupéry', 1943),
+('La Peste', 'Albert Camus', 1947);
